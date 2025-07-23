bcb219be21ddbde8c08a092f228746e36c733fe8
CRM-group3 <tekarchgroup3@gmail.com>
2025-07-23 09:34:38 -0700

    adding xpath

--- src/test/java/com/group3/CRMpages/AddProductPage.java
+++ src/test/java/com/group3/CRMpages/AddProductPage.java
@@ -1,5 +1,21 @@
 package com.group3.CRMpages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class AddProductPage {
+	WebDriver driver;
+	public  AddProductPage(WebDriver driver) {
+	    this.driver = driver;
+	    PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath="//a[text()=\"Products\"]")
+	public WebElement productsPage;
+
+	
+	
 
 }
