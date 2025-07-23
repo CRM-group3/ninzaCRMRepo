f769f75c4241c01d037cc10f9895bcc3d19ab735
darshanarn26 <darshanarn26@gmail.com>
2025-07-23 12:08:19 -0500

    WIP: saving changes before switching to main

--- src/main/java/com/group3/CRMbasics/BaseTest.java
+++ src/main/java/com/group3/CRMbasics/BaseTest.java
@@ -1,5 +1,31 @@
 package com.group3.CRMbasics;
 
-public class BaseTest {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseTest 
+{
+	WebDriver driver;
+	
+	public WebDriver getDriver()
+	{
+		if(driver==null)
+		{
+			WebDriverManager.chromedriver().setup();
+			driver=new ChromeDriver();
+			driver.get("");
+			driver.manage().window().maximize();
+		}
+		return driver;
+	}
+
+	@AfterMethod
+	public void teardown()
+	{
+		driver.close();
+		driver=null;
+	}
 }
