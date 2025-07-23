d1570137871a81c9a4547473bf8613e380eeee59
“Sneha <snehalohia2014@gmail.com>
2025-07-23 08:38:06 -0700

    added utilities

--- src/main/java/com/group3/CRMbasics/BaseTest.java
+++ src/main/java/com/group3/CRMbasics/BaseTest.java
@@ -1,5 +1,26 @@
 package com.group3.CRMbasics;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BaseTest {
+	
+WebDriver driver;
+	
+	public WebDriver getDriver() {
+		if(driver == null) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+		return driver;
+	}
+	
+	
+	public void close() {
+		driver.close();
+		driver = null;
+	}
 
 }
