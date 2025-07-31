b83b9c870dbda022597e7e8cbf6b115fb34a1d1b
Chitra C <ChitraC@tekarch.com>
2025-07-31 09:52:42 -0700

    added two files for usertest scripts

--- src/test/java/com/group3/CRMpages/CreateUserPage.java
+++ src/test/java/com/group3/CRMpages/CreateUserPage.java
@@ -2,13 +2,18 @@ package com.group3.CRMpages;
 
 
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class CreateUserPage {
     WebDriver driver;
     Actions actions;
@@ -19,25 +24,30 @@ public class CreateUserPage {
         actions = new Actions(driver);
     }
 
-    @FindBy(id = "firstName")
+    @FindBy(name = "empName")
     private WebElement firstNameInput;
 
-    @FindBy(id = "lastName")
-    private WebElement lastNameInput;
+    @FindBy(name = "mobileNo")
+    private WebElement mobileNumber;
 
-    @FindBy(id = "email")
+    @FindBy(name = "email")
     private WebElement emailInput;
+    @FindBy(name = "username")
+    private WebElement userName;
 
-    @FindBy(id = "password")
+    @FindBy(name = "password")
     private WebElement passwordInput;
 
-    @FindBy(id = "submitBtn")
+    @FindBy(xpath="//button[text()='Create User']")
     private WebElement submitBtn;
     @FindBy(xpath = "//div[@class='nav-link' and contains(., 'Admin Console')]")
     private WebElement adminMenu;
 
     @FindBy(xpath = "//div[@class='dropdown-item' and text()='Create User']")
     private WebElement createUserLink;
+    @FindBy(xpath ="//div[contains(text(), 'Successfully')]")
+    private WebElement alertMsg;
+   
     
     public void navigateToCreateUser() {
         actions.moveToElement(adminMenu).perform();       // Hover on Admin menu
@@ -48,26 +58,32 @@ public class CreateUserPage {
         firstNameInput.sendKeys(firstName);
     }
 
-    public void enterLastName(String lastName) {
-        lastNameInput.sendKeys(lastName);
+    public void entermobileNumber(String mobile) {
+        mobileNumber.sendKeys(mobile);
     }
 
     public void enterEmail(String email) {
         emailInput.sendKeys(email);
     }
-
+    public void enterUserName(String username) {
+        userName.sendKeys(username);
+    }
     public void enterPassword(String password) {
         passwordInput.sendKeys(password);
     }
-
+	/* public String getAlertMessage(String alertMsg) { 
+	return"";
+	
+}*/
     public void clickSubmit() {
         submitBtn.click();
     }
 
-    public void createUser(String firstName, String lastName, String email, String password) {
+    public void createUser(String firstName, String mobilenumber, String email, String username,String password) {
         enterFirstName(firstName);
-        enterLastName(lastName);
+        entermobileNumber(mobilenumber);
         enterEmail(email);
+        enterUserName(username);
         enterPassword(password);
         clickSubmit();
     }
