07e6aa58ba5d518babdb55caf7ef6d7565043312
CRM-group3 <tekarchgroup3@gmail.com>
2025-08-01 19:30:51 -0500

    commit

--- src/test/java/com/group3/CRMpages/OpportunityPage.java
+++ src/test/java/com/group3/CRMpages/OpportunityPage.java
@@ -1,14 +1,130 @@
 package com.group3.CRMpages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
 
 import com.group3.CRMbasics.BasePage;
 
 public class OpportunityPage extends BasePage{
 
-	public OpportunityPage(WebDriver driver) {
+	public OpportunityPage(WebDriver driver) 
+	{
 		super(driver);
-		// Auto-generated constructor stub
+		
 	}
 
+	public @FindBy(xpath="//a[@class='nav-link' and text()='Opportunities']")
+	WebElement opportunity;
+	
+	public @FindBy(xpath="//span[contains(text(),'Create Opportunity')]")
+	WebElement createoppo;
+	
+	public @FindBy(xpath="//input[@value='Auto Generated']")
+	WebElement oppoid;
+	
+	public @FindBy(xpath="//input[@name='opportunityName']")
+	WebElement opponame;
+	
+	public @FindBy(xpath="//button[@type='submit']")
+	WebElement submit;
+		
+	public @FindBy(xpath="//input[@name='amount']")
+	WebElement amount;
+	
+	public @FindBy(xpath="//input[@name='businessType']")
+	WebElement businesstype;
+	
+	public @FindBy(xpath="//input[@name='nextStep']")
+	WebElement nextstep;
+	
+	public @FindBy(xpath="//input[@name='salesStage']")
+	WebElement salesstage;
+	
+	public void clickOpportunity() throws InterruptedException
+	{
+		Thread.sleep(2000);
+		opportunity.click();
+		
+	}
+	
+	public void createopportunity()
+	{
+		createoppo.click();
+	}
+	
+	public void checkoppoid()
+	{
+		
+		 String actualValue = oppoid.getAttribute("value");  
+	        System.out.println("Opportunity ID Value: " + actualValue);
+	        Assert.assertEquals(actualValue, "Auto Generated", 
+	                "Opportunity ID is not auto-generated as expected.");
+	}
+
+	
+	public void opponamecheck()
+	{
+		opponame.clear();
+		opponame.sendKeys("");
+		submit.click();
+		String actualvalue=opponame.getText(); 
+		System.out.println("Error message : Please fill out field" + actualvalue);
+		Assert.assertEquals(actualvalue, "Name is empty", 
+                "Error message");
+	}
+	
+	public void amountcheck()
+	{
+		amount.clear();
+		amount.sendKeys("");
+		submit.click();
+		String actualvalue=amount.getText();
+		System.out.println("Error message : Please fill out field" + actualvalue);
+		Assert.assertEquals(actualvalue, "Amount is empty", 
+                "Error message");
+		amount.sendKeys("abcd");
+		String actualvalue2=amount.getText();
+		System.out.println("Error message : Amount should be numeric" + actualvalue2);
+		Assert.assertEquals(actualvalue, "Amount should be numeric", 
+                "Error message");
+	}
+	
+	public void businesstypecheck()
+	{
+		businesstype.clear();
+		businesstype.sendKeys("");
+		submit.click();
+		String actualvalue=businesstype.getText();
+		System.out.println("Error message : Please fill out field" + actualvalue);
+		Assert.assertEquals(actualvalue, "businesstype is empty", 
+                "Error message");
+	}
+	
+	public void nextstep()
+	{
+		nextstep.clear();
+		nextstep.sendKeys("");
+		submit.click();
+		String actualvalue=nextstep.getText();
+		System.out.println("Error message : Please fill out field" + actualvalue);
+		Assert.assertEquals(actualvalue, "nextstep is empty", 
+                "Error message");
+		
+	}
+	
+	public void checksalesstage()
+	{
+		salesstage.clear();
+		salesstage.sendKeys("");
+		submit.click();
+		String actualvalue=salesstage.getText();
+		System.out.println("Error message : Please fill out field" + actualvalue);
+		Assert.assertEquals(actualvalue, "salestage is empty", 
+                "Error message");
+	}
 }
+
+
+
--- src/test/java/com/group3/CRMtests/OpportunityTest.java
+++ src/test/java/com/group3/CRMtests/OpportunityTest.java
@@ -1,21 +1,127 @@
 package com.group3.CRMtests;
 
-import org.openqa.selenium.WebDriver;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.group3.CRMbasics.BaseTest;
-import com.group3.CRMutilities.PropertiesFile;
-import com.group3.CRMutilities.ScreenShots;
+import com.group3.CRMlistners.ExtentManager;
+import com.group3.CRMlogs.Logs;
+import com.group3.CRMpages.OpportunityPage;
+
 
 public class OpportunityTest extends BaseTest{
-	WebDriver driver;
-	PropertiesFile prop;
-	ScreenShots screensht = new ScreenShots();
+	
+	
+	OpportunityPage oppo;
+	@Test public void opportunity() throws InterruptedException 
+	{
+	 ExtentManager.logTestInfo("Opening opportunity Page"); 
+	Logs.info("OPening opportunity Page"); 
+	oppo=new OpportunityPage(driver); 
+	basepage.waitForElement(oppo.opportunity, Duration.ofSeconds(2000)); 
+	oppo.clickOpportunity(); 
+	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); 
+	WebElement header = wait.until(ExpectedConditions .visibilityOfElementLocated(By.xpath("//h1[contains(text(),'Opportunities')]"))); 
+	Assert.assertTrue(driver.getCurrentUrl().contains("/opportunities"), "Failed to navigate to Opportunities page"); 
+	
+	}
 	
 	@Test
-	public void test() {
-		System.out.println("I am in a test");
+	public void createoppo() throws InterruptedException
+	{
+		
+		oppo=new OpportunityPage(driver);
+		Thread.sleep(2000);
+		oppo.clickOpportunity();
+		oppo.createopportunity();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+	       WebElement header = wait.until(ExpectedConditions
+	         .visibilityOfElementLocated(By.xpath("//h1[contains(text(),'Opportunities')]")));
+	   
+	   Assert.assertTrue(driver.getCurrentUrl().contains("/opportunities"),
+		        "Failed to navigate to Opportunities page");
+	   
+	   driverClose();
+	   
 	}
+	
+	@Test
+	public void oppoIdAutogeneration() throws InterruptedException
+	{
+		oppo=new OpportunityPage(driver);
+		Thread.sleep(2000);
+		oppo.clickOpportunity();
+		oppo.createopportunity();
+		oppo.checkoppoid();
+	}
+	
+	
+	@Test
+	public void opponamecheck() throws InterruptedException
+	{
+		oppo=new OpportunityPage(driver);
+		Thread.sleep(2000);
+		oppo.clickOpportunity();
+		oppo.createopportunity();
+		oppo.opponamecheck();
+	}
+	
+	@Test
+	public void amountcheck() throws InterruptedException
+	{
+		oppo=new OpportunityPage(driver);
+		Thread.sleep(2000);
+		oppo.clickOpportunity();
+		Thread.sleep(2000);
+		oppo.createopportunity();
+		Thread.sleep(2000);
+		oppo.amountcheck();
+	}
+	
+	@Test
+	public void businesstypecheck() throws InterruptedException
+	{
+		oppo=new OpportunityPage(driver);
+		Thread.sleep(2000);
+		oppo.clickOpportunity();
+		Thread.sleep(2000);
+		oppo.createopportunity();
+		Thread.sleep(2000);
+		oppo.businesstypecheck();
+	}
+	
+	@Test
+	public void nextstage() throws InterruptedException
+	{
+		oppo=new OpportunityPage(driver);
+		Thread.sleep(2000);
+		oppo.clickOpportunity();
+		Thread.sleep(2000);
+		oppo.createopportunity();
+		Thread.sleep(2000);
+		oppo.nextstep();
+	}
+	
+	@Test
+	public void salestage() throws InterruptedException
+	{
+		oppo=new OpportunityPage(driver);
+		Thread.sleep(2000);
+		oppo.clickOpportunity();
+		Thread.sleep(2000);
+		oppo.createopportunity();
+		Thread.sleep(2000);
+		oppo.checksalesstage();
+	}
+
 	
 		
 }
